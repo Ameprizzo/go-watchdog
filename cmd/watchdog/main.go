20f0e426ad94774b92a2fcdeca6fae805c94eeba
Amedeus Lyakurwa <amedeus926@gmail.com>
2026-01-08 13:08:06 +0300

    Implement main program logic and configuration loading for monitoring sites

--- cmd/watchdog/main.go
+++ cmd/watchdog/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/Ameprizzo/go-watchdog/internal/monitor"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	fmt.Println("🐕 Go-Watchdog is starting...")
+
+	//Load configuration
+	cfg, err := monitor.LoadConfig("config.json")
+	if err != nil {
+		log.Fatalf("Error loading config: %v", err)
+	}
+
+	// Print loaded SITES for verification
+	fmt.Printf("Monitoring %d sites:\n", len(cfg.Sites))
+	for _, site := range cfg.Sites {
+		fmt.Printf("- %s (%s)\n", site.Name, site.URL)
+	}
+
 }
--- internal/monitor/monitor.go
+++ internal/monitor/monitor.go
@@ -1,5 +1,10 @@
 package monitor
 
+import (
+	"encoding/json"
+	"os"
+)
+
 // Config represents the entire config file
 type Config struct {
 	Settings Settings `json:"settings"`
@@ -17,4 +22,21 @@ type Site struct {
 	Name   string `json:"name"`
 	URL    string `json:"url"`
 	Status string // This will be used later (Up/Down)
+}
+
+// LoadConfig reads and parses the config file
+func LoadConfig(filePath string) (*Config, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var config Config
+	decoder := json.NewDecoder(file)
+	if err := decoder.Decode(&config); err != nil {
+		return nil, err
+	}
+
+	return &config, nil
 }
