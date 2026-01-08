f94f6f9dd7c2ef78c4c62d2f5cccc836bf218ca5
Amedeus Lyakurwa <amedeus926@gmail.com>
2026-01-08 13:50:15 +0300

    Refactor main logic for site checks and update configuration interval to 10 seconds

--- cmd/watchdog/main.go
+++ cmd/watchdog/main.go
@@ -9,36 +9,44 @@ import (
 )
 
 func main() {
-	fmt.Println("🐕 Go-Watchdog is starting...")
-
-	//Load configuration
 	cfg, err := monitor.LoadConfig("config.json")
 	if err != nil {
-		log.Fatalf("Error loading config: %v", err)
+		log.Fatalf("Error: %v", err)
 	}
 
-	// Create a channel to receive results
+	// Create a ticker based on our config interval
+	ticker := time.NewTicker(time.Duration(cfg.Settings.CheckInterval) * time.Second)
+
+	fmt.Printf("🐕 Watchdog started. Checking %d sites every %ds...\n",
+		len(cfg.Sites), cfg.Settings.CheckInterval)
+
+	// Run immediately on start
+	runChecks(cfg)
+
+	// Keep the program running and check on every "tick"
+	for range ticker.C {
+		runChecks(cfg)
+	}
+}
+
+func runChecks(cfg *monitor.Config) {
+	fmt.Printf("\n--- Check started at %s ---\n", time.Now().Format("15:04:05"))
+
 	results := make(chan monitor.Result)
 
-	fmt.Println("🐕 Watchdog active. Starting checks...")
-
-	// Launch a goroutine for each site
 	for _, site := range cfg.Sites {
-		fmt.Printf("- %s (%s)\n", site.Name, site.URL)
 		go func(s monitor.Site) {
-			result := monitor.CheckSite(s, cfg.Settings.Timeout)
-			results <- result
+			results <- monitor.CheckSite(s, cfg.Settings.Timeout)
 		}(site)
 	}
 
-	//Collect and print results as they come in
 	for i := 0; i < len(cfg.Sites); i++ {
 		res := <-results
-		status := "✅ UP"
+		icon := "✅"
 		if !res.IsUp {
-			status = "❌ DOWN"
+			icon = "❌"
 		}
-		fmt.Printf("[%s] %-15s | Latency: %v | Status: %d\n", status, res.Name, res.Latency.Round(time.Millisecond), res.StatusCode)
-
+		fmt.Printf("%s %-15s | Latency: %v | Code: %d\n",
+			icon, res.Name, res.Latency.Round(time.Millisecond), res.StatusCode)
 	}
 }
